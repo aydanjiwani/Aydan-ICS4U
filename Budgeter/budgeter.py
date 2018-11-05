6dfa62d5293036ee672312b9b5e1b6dda31038b0
helpmynamewontfi <aydan.jiwani@gmail.com>
2018-11-04 19:54:40 -0500

    Added basic recommendation system and checking rankings

--- Budgeter/budgeter.py
+++ Budgeter/budgeter.py
@@ -6,8 +6,9 @@ from alpha_vantage.techindicators import TechIndicators
 from pprint import pprint
 
 
-
-
+f2 = open("STOCKLIST.txt", "r")
+stocks = f2.read().splitlines()
+stockindex = [Stock(stocks[i],0,0,0) for i in range (0,stocks.len())]
 
 billOne = Bill("Student Loan", 250, 0.1, 10000)
 billOne.showSchedule()
@@ -15,4 +16,30 @@ billOne.increasePayment(500)
 billOne.showSchedule()
 StockOne = Stock("GOOG",0, 0, 0)
 StockOne.getInfo()
-
+def recommend(criteria):
+	
+	
+		if(criteria == 1):
+			print("finding stocks with highest value")
+			for i in range (0,stocks.len()):
+				stockindex[i].getInfo()
+			stocksByValue = sorted(stocks, key=lambda x: x.value, reverse=True)
+			print("The stocks with the best value are:" + stocksByValue[0].name + stocksByValue[1].name + stocksByValue[2].name)
+			
+			
+			
+			
+			
+		if(criteria == 2):
+			print("finding stocks with best trend")
+			for i in range (0,stocks.len()):
+				stockindex[i].getInfo()
+			stocksByTrend = sorted(stocks, key=lambda x: (x.sma-x.value), reverse=True)
+			print("The stocks with the best trend are:" + stocksByTrend[0].name + stocksByTrend[1].name + stocksByTrend[2].name)
+			
+		if(criteria == 3):
+			print("finding stock with best RSI")
+			for i in range (0,stocks.len()):
+				stockindex[i].getInfo()
+			stocksByRSI = sorted(stocks, key=lambda x: x.rsi, reverse=True)
+			print("The stocks with the best RSI are:" + stocksByRSI[0].name + stocksByRSI[1].name + stocksByRSI[2].name)
--- Budgeter/stock.py
+++ Budgeter/stock.py
@@ -1,6 +1,9 @@
 from alpha_vantage.timeseries import TimeSeries
 from alpha_vantage.techindicators import TechIndicators
 from pprint import pprint
+from binarysearch import binarySearch
+
+
 
 class Stock():
 """
@@ -51,17 +54,50 @@ An object that holds the information of a selected stock
 		none
                 
 		'''
+		
         f = open("stockinfo.txt", "w")
         ts = TimeSeries(key='GZR6JEGOB3LF6RCB')
         tc = TechIndicators(key='GZR6JEGOB3LF6RCB')
         data, meta_data = ts.get_daily(self.name)
         data2, meta_data2 = tc.get_rsi(self.name)
         data3, meta_data3 = tc.get_sma(self.name)
-        teststring = data['2018-10-12']
+		self.rsi = float(data2['2018-11-04'])
+		self.sma = float(data3['2018-11-04'])
+        teststring = data['2018-11-04']
         print(teststring)
         self.value = float(teststring['4. close'])
         f.write("The stock chosen is: " + self.name + "\n")
         f.write("The most recent closing price is: " + str(self.value) + "\n")
         print("stock info added to file")
+	def checkRanking(self, criteria):
+		f2 = open("STOCKLIST.txt", "r")
+		stocks = f2.read().splitlines()
+		stockindex = [Stock(stocks[i],0,0,0) for i in range (0,stocks.len())]
+		values = []
+		if(criteria = 1):
+			key = self.value
+			for i in range (0,stocks.len()):
+				stockindex[i].getInfo()
+				values.append(stockindex[i].value)
+		ranking = binarySearch(values, stockindex.len(), 0, key)
+		print(self.name + "is ranked" + ranking) 
+		return ranking
+				
+			
+		if(criteria = 2):
+			key = self.sma-self.value
+			for i in range (0, stocks.len()):
+				if(key = stockindex[i].sma-stockindex[i].value):
+					print(self.name + "is ranked" + i) 
+					return i
+		if(criteria = 3):
+			key = self.rsi
+			for i in range (0, stocks.len()):
+				if(key = stockindex[i].rsi):
+					print(self.name + "is ranked" + i) 
+					return i
+		
+		
+		
 
 
